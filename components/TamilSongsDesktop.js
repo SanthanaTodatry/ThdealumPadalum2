e3b6a5079ce8377e4a5f98182d13628881d82ec2
Santo <51939359+SanthanaTodatry@users.noreply.github.com>
2025-06-13 15:51:15 -0500

    Update TamilSongsDesktop.js

--- components/TamilSongsDesktop.js
+++ components/TamilSongsDesktop.js
@@ -427,7 +427,7 @@ const TamilSongsVisualization = () => {
       
       <div className="flex flex-1 overflow-hidden">
         {/* Panel 1: Filters - INCREASED WIDTH */}
-        <div className="w-80 bg-white border-r border-blue-200 flex flex-col">
+        <div className="bg-white border-r border-blue-200 flex flex-col" style={{ width: '400px', minWidth: '400px' }}>                
           <div className="p-4 border-b border-blue-200">
             {/* Reset Button - RENAMED */}
             <button
